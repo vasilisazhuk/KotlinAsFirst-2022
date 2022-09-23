a508941f7c8c083b1a7d3fbae1f3dbde00fb394b
Василиса <zhookvasilisad@gmail.com>
2022-09-23 15:48:05 +0300

    решены задания IfElse

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -67,8 +67,8 @@ fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
 ): Boolean {
-    if (x1 == x2 && y1 == y2) return true
-    else if (sqrt(sqr(x1 - x2) + sqr(y1 - y2)) == (r1 + r2)) return true
+    if ((x1 == x2 && y1 == y2) && (r1 <= r2)) return true
+    else if (sqrt(sqr(x1 - x2) + sqr(y1 - y2)) <= (r2 - r1)) return true
     else return false
 
 }
