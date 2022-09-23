c60dd0316c1dc162807d63a6d358b1bd0a20fecb
Василиса <zhookvasilisad@gmail.com>
2022-09-23 12:59:45 +0300

    решены задания IfElse

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -83,7 +83,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
 
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg + min / 60 + sec / 3600) * PI / 180
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg + min / 60.0 + sec / 3600.0) * PI / 180
 
 /**
  * Тривиальная (1 балл)
